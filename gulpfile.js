ae7dd70be2599b336011c53ee9f39ec22a977845
Marco Carvalho <marcolucio27@gmail.com>
2017-12-07 01:29:08 -0200

    deleting useless files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-
-gulp.task('pug', () => {
-    gulp.src(['**/*.pug', '!node_modules/**/*'])
-        .pipe(pug())
-        .pipe(gulp.dest('.'));
-});
