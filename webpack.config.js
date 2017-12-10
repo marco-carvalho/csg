d72f0ccf54cf8e981c93ac3220d828c6f05954d8
Marco Carvalho <marcolucio27@gmail.com>
2017-12-10 19:49:52 -0200

    remove useless config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
     entry: {
         app: './src/app.js'
     },
@@ -22,10 +20,6 @@ module.exports = {
         }]
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
         new UglifyJSPlugin()
     ]
 }
