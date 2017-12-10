36ae5ca86bd41a8bb176bfef1fa2dbf3a05099ea
Marco Carvalho <marcolucio27@gmail.com>
2017-12-10 21:12:47 -0200

    update examples

--- src/examples/1.ts
+++ src/examples/1.ts
@@ -3,24 +3,24 @@ import { ThreeCSG } from "../threeCSG";
 
 const group = new THREE.Group();
 
-const boxMesh = [
+const boxesMesh = [
     new THREE.Mesh(new THREE.BoxGeometry(3, 1, 1)),
     new THREE.Mesh(new THREE.BoxGeometry(1, 3, 1)),
     new THREE.Mesh(new THREE.BoxGeometry(1, 1, 3)),
     new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3))
 ];
 
-const sphereMesh = [
+const spheresMesh = [
     new THREE.Mesh(new THREE.SphereGeometry(2, 32, 32))
 ];
 
-const boxWithWidth = new ThreeCSG(boxMesh[0]);
+const boxWithWidth = new ThreeCSG(boxesMesh[0]);
 let result = boxWithWidth.toMesh(new THREE.MeshLambertMaterial());
 result.position.x = -8;
 result.position.y = -8;
 group.add(result);
 
-const boxWithHeight = new ThreeCSG(boxMesh[1]);
+const boxWithHeight = new ThreeCSG(boxesMesh[1]);
 result = boxWithHeight.toMesh(new THREE.MeshLambertMaterial());
 result.position.x = -4;
 result.position.y = -8;
@@ -32,19 +32,19 @@ result.position.x = -6;
 result.position.y = -2;
 group.add(result);
 
-const boxWithDepth = new ThreeCSG(boxMesh[2]);
+const boxWithDepth = new ThreeCSG(boxesMesh[2]);
 result = boxWithDepth.toMesh(new THREE.MeshLambertMaterial());
 result.position.x = -2;
 result.position.y = -2;
 group.add(result);
 
-const sphere = new ThreeCSG(sphereMesh[0]);
+const sphere = new ThreeCSG(spheresMesh[0]);
 result = sphere.toMesh(new THREE.MeshLambertMaterial());
 result.position.x = 2;
 result.position.y = -2;
 group.add(result);
 
-const box = new ThreeCSG(boxMesh[3]);
+const box = new ThreeCSG(boxesMesh[3]);
 result = box.toMesh(new THREE.MeshLambertMaterial());
 result.position.x = 6;
 result.position.y = -2;
--- src/examples/2.ts
+++ src/examples/2.ts
@@ -3,28 +3,28 @@ import { ThreeCSG } from "../threeCSG";
 
 const group = new THREE.Group();
 
-const sphereMesh = new THREE.Mesh(new THREE.SphereGeometry(2, 32, 8));
-const boxMesh = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3));
+const sphere = new THREE.SphereGeometry(2, 32, 8);
+const sphereMesh = new THREE.Mesh(sphere);
 sphereMesh.position.x = 2;
 sphereMesh.position.y = 2;
 sphereMesh.position.z = 2;
+const sphereCSG = new ThreeCSG(sphereMesh);
 
-const CSG = [
-    new ThreeCSG(sphereMesh),
-    new ThreeCSG(boxMesh)
-];
+const box = new THREE.BoxGeometry(3, 3, 3);
+const boxMesh = new THREE.Mesh(box);
+const boxCSG = new ThreeCSG(boxMesh);
 
-let result = CSG[0].union(CSG[1]).toMesh(new THREE.MeshLambertMaterial());
+let result = sphereCSG.union(boxCSG).toMesh(new THREE.MeshLambertMaterial());
 result.position.x = 8;
 result.position.y = 0;
 group.add(result);
 
-result = CSG[1].subtract(CSG[0]).toMesh(new THREE.MeshLambertMaterial());
+result = boxCSG.subtract(sphereCSG).toMesh(new THREE.MeshLambertMaterial());
 result.position.x = 0;
 result.position.y = -2;
 group.add(result);
 
-result = CSG[0].intersect(CSG[1]).toMesh(new THREE.MeshLambertMaterial());
+result = sphereCSG.intersect(boxCSG).toMesh(new THREE.MeshLambertMaterial());
 result.position.x = -6;
 result.position.y = 0;
 group.add(result);
