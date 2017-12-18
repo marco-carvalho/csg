d8ade53f7510d80f9ec5f38fcd6a5586981f9a7f
Marco Carvalho <marcolucio27@gmail.com>
2017-12-18 16:35:10 -0200

    from ts-loader to babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
-// const path = require('path');
-// const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const path = require("path");
+// const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
     entry: {
-        app: './app.ts'
+        app: "./app.js"
     },
     output: {
         path: __dirname,
-        filename: 'bundle.js'
+        filename: "bundle.js"
     },
     module: {
         loaders: [
             {
                 test: /\.pug$/,
-                loader: 'pug-loader'
+                loader: "pug-loader"
             },
             {
-              test: /\.ts$/,
-              use: 'ts-loader',
-              exclude: /node_modules/
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env"]
+                }
             }
         ]
     },
     resolve: {
-      extensions: ['.ts', '.js']
+        extensions: [
+            ".ts",
+            ".js"
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.pug'
-        }),
+            template: "./index.pug"
+        })
         // new UglifyJSPlugin()
     ]
-}
+};
