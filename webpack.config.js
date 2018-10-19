7beb226e4bbd54163caf484624c7236d6bfe8e56
Marco Carvalho <marcolucio27@gmail.com>
2018-10-19 15:32:52 -0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,60 @@
-// const path = require("path");
-// const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-    entry: {
-        app: "./app.js"
-    },
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.pug$/,
-                loader: "pug-loader"
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets: ["env"]
-                }
-            }
+  mode: "development",
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/u,
+        loader: "pug-loader",
+        query: {}
+      },
+      {
+        test: /\.js$/u,
+        exclude: /node_modules/u,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      },
+      {
+        test: /\.scss$/u,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
         ]
-    },
-    resolve: {
-        extensions: [
-            ".ts",
-            ".js"
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/u,
+        use: "url-loader?limit=10000"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/u,
+        use: "file-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/u,
+        use: [
+          "file-loader?name=images/[name].[ext]",
+          "image-webpack-loader?bypassOnDebug"
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.pug"
-        })
-        // new UglifyJSPlugin()
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.pug"
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 };
